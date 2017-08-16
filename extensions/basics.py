e8d5a3b3a09f4a14d435dc9a9221f1da29c0d334
SombreroCat <darin.kuo@gmail.com>
2017-08-16 13:06:46 -0700

    changed return message from code block into embeded message format. Removed ctx and pass_context=True from .roll command

--- extensions/basics.py
+++ extensions/basics.py
@@ -11,27 +11,29 @@ class Basic:
     @kobold.command(pass_context=True)
     async def hello(self,ctx):
         """Hello : .kobold greets the user that calls the command."""
-        embed = discord.Embed(colour=discord.Colour.blue())
+        embed = discord.Embed(colour=discord.Color.blue())
         embed.title = "Greetings"
         embed.add_field(name="Greeting", value="Hello " + ctx.message.author.mention + ", how are you today?")
         await self.kobold.say(embed=embed)
  #       await self.kobold.say("Hello " + ctx.message.author.mention + ", how are you today?")
 
-    @kobold.command(pass_context=True)
-    async def roll(self,ctx,dice:str="1d4",bonus:str="0"):
+    @kobold.command()
+    async def roll(self,dice:str="1d4",bonus:str="0"):
         """Roll : rolls a dice type the user inputs in a 1d4 format
            and adds/subtracts bonuses from the roll.Prints out into
            the text channel the command was invoked. Default die is
            1d4 and bonus default is 0."""
         r = Dice(dice,bonus)
         r.add_dice()
-        if r.faces == 20:
-            await  self.kobold.say(ctx.message.author.mention + "```" + "\nDice type: d" + str(r.faces) +
-                      "\nAmount of rolls: " + str(r.rolls) + "\nCrit success(es): " + str(r.crit_success) +
-                      "\nCrit failure(s): " + str(r.crit_fail) + "\nBonus: " + bonus + "\nTotal: " + str(r.total_amount) + "```")
-        else:
-            await  self.kobold.say(ctx.message.author.mention + "```" + "\nDice type: d" + str(r.faces) +
-                                   "\nAmount of rolls: " + str(r.rolls) + "\nBonus: " + bonus + "\nTotal: " + str(r.total_amount) + "```")
+        embed = discord.Embed(color=discord.Color.dark_red())
+        embed.title = "Dice"
+        embed.description = str(r.rolls) + "d" + str(r.faces)
+        embed.add_field(name="Roll(s)",value=r.adding_string)
+        embed.add_field(name="Bonus",value=bonus)
+        embed.add_field(name="Total",value=r.total_amount)
+        embed.add_field(name="Crit Success", value=str(r.crit_success))
+        embed.add_field(name="Crit Fails",value=str(r.crit_fail))
+        await self.kobold.say(embed=embed)
 
     @kobold.command(pass_context=True)
     async def dmroll(self,ctx,dice:str="1d4",bonus:str="0"):
@@ -39,16 +41,18 @@ class Basic:
            and adds/subtracts bonuses from the roll and send the result
            directly to the user. Perfect for behind the screen rolls.
            Default die is 1d4 and bonus default is 0."""
-        user = ctx.message.author
         r = Dice(dice, bonus)
         r.add_dice()
-        if r.faces == 20:
-            await self.kobold.send_message(user,"```" + "\nDice type: d" + str(r.faces) +
-                                   "\nAmount of rolls: " + str(r.rolls) + "\nCrit success(es): " + str(r.crit_success) +
-                                   "\nCrit failure(s): " + str(r.crit_fail) + "\nBonus: " + bonus + "\nTotal: " + str(r.total_amount) + "```")
-        else:
-            await self.kobold.send_message(user,"```" + "\nDice type: d" + str(r.faces) +
-                                  "\nAmount of rolls: " + str(r.rolls) + "\nBonus: " + bonus + "\nTotal: " + str(r.total_amount) + "```")
+        embed = discord.Embed(color=discord.Color.dark_red())
+        embed.title = "Dice"
+        embed.description = str(r.rolls) + "d" + str(r.faces)
+        embed.add_field(name="Roll(s)",value=r.adding_string)
+        embed.add_field(name="Bonus",value=bonus)
+        embed.add_field(name="Total",value=r.total_amount)
+        embed.add_field(name="Crit Success", value=str(r.crit_success))
+        embed.add_field(name="Crit Fails",value=str(r.crit_fail))
+        user = ctx.message.author
+        await self.kobold.send_message(user,embed=embed)
 
     @kobold.command()
     async def coinflip(self):
@@ -64,6 +68,7 @@ class Dice:
         self.total_amount = 0
         self.crit_fail = 0
         self.crit_success = 0
+        self.adding_string = ""
 
     def add_dice(self):
         for x in range(0, self.rolls):
@@ -74,7 +79,14 @@ class Dice:
                 elif roll == 20:
                     self.crit_success += 1
             self.total_amount += roll
+            self.add_string(x,str(roll))
         self.total_amount += int(self.bonus)
+
+    def add_string(self,num,roll):
+        if num==0:
+            self.adding_string += roll
+        else:
+            self.adding_string += "+" + roll
 
 def setup(kobold):
     kobold.add_cog(Basic(kobold))
