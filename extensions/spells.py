2545013a8ba339715453c5faaea18ec6afd01d32
AwfulPug <darinkuo@gmail.com>
2017-09-25 21:33:15 -0700

    Improved formatting for embed spell calling. Need to find a way to reduce repeating add_field and including all fields since variation between spells.

--- extensions/spells.py
+++ extensions/spells.py
@@ -1,6 +1,4 @@
 import requests
-import aiohttp
-import json
 import discord
 from discord.ext import commands as kobold
 
@@ -16,14 +14,24 @@ class Spells:
         formatted= caps.replace(" ","+")
 
         jdata = requests.get(base_url + "spells/?name=" + formatted).json()
-        results = jdata["results"][0]
-        url = results["url"]
+        if not jdata["results"]:
+            await self.kobold.say("Spell Not Found")
+        else:
+            results = jdata["results"][0]
+            url = results["url"]
 
-        jdata = requests.get(url).json()
-        embed = discord.Embed(colour=discord.Color.blue())
-        embed.title = jdata["name"]
-
-        await self.kobold.say(embed=embed)
+            jdata = requests.get(url).json()
+            embed = discord.Embed(colour=discord.Color.blue())
+            embed.title = jdata["name"]
+            embed.description = jdata["desc"][0]
+            embed.add_field(name="Higher Levels",value=jdata["higher_level"][0])
+            embed.add_field(name="Range",value=jdata["range"])
+            embed.add_field(name="Components",value=jdata["components"])
+            embed.add_field(name="Cast Time",value=jdata["casting_time"])
+            embed.add_field(name="Ritual",value=jdata["ritual"])
+            embed.add_field(name="Duration",value=jdata["duration"])
+            embed.add_field(name="Concentration",value=jdata["concentration"])
+            await self.kobold.say(embed=embed)
 
 def setup(kobold):
     kobold.add_cog(Spells(kobold))
