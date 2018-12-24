aa7d4f52331ed60d3cbe466b42d10b448135cc25
Darin Kuo <kuodm@uci.edu>
2018-12-24 01:44:43 -0800

    changed how kobold processes bot tokens.

--- kobold.py
+++ kobold.py
@@ -8,22 +8,26 @@ extensions = {
     "extensions.dice",
     "extensions.spells"
 }
+
+# reading the bot key and storing it in variable "key"
+token = open("Token.txt", "r")
+key = token.readline()
+token.close()
+
 kobold = commands.Bot(description=description,command_prefix=command_prefix)
 
 @kobold.event
 async def on_ready():
     """ Called when the client is done preparing data recieved from Discord. """
-    print("Logged In.")
-    print("Name : {}".format(kobold.user.name))
-    print("ID : {}".format(kobold.user.id))
-    print("Discord Ver : {}".format(discord.__version__))
+    print(f"App Name: {kobold.user.name}\nBot ID: {kobold.user.id}\nversion {discord.__version__}")
 
     for extension in extensions:
         try:
             kobold.load_extension(extension)
-    # If an exception is thrown, that exception is bound to the variable e.
+    # If an exception is thrown, that exception is bound to the variable x.
         except Exception as x:
-            exc = "{}:{}".format(type(x).__name__,x)
+            exc = "{}:{}".format(type(x).__name__, x)
             print("Unable to load extension {}\n{}".format(extension, exc))
-key = "NDIzMjQyNTk0ODU2OTkyNzg4.DwItmA.oYn8aBJeVhib5DBCEjccb1J5oBA"
+
+    print("==============\nApplication Online and Running")
 kobold.run(key)
